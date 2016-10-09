99ebb0323bb6462c0dc99fe9baaaf3c9e333496c
muldrewd <muldrewd@yahoo.com>
2016-10-09 12:28:34 -0700

    buzzer music refinements

--- chair/buzzer_music.py
+++ chair/buzzer_music.py
@@ -1,6 +1,47 @@
 import RPi.GPIO as GPIO   #import the GPIO library
 import time               #import the time library
 
+
+# NOTES TO FREQUENCIES DICTIONARY
+fN =  {
+            "cL": 129,
+            "cLS": 139,
+            "dL": 146,
+            "dLS": 156,
+            "eL": 163,
+            "fL": 173,
+            "fLS": 185,
+            "gL": 194,
+            "gLS": 207,
+            "aL": 219,
+            "aLS": 228,
+            "bL": 232,
+            "c": 261,
+            "cS": 277,
+            "d": 294,
+            "dS": 311,
+            "e": 329,
+            "f": 349,
+            "fS": 370,
+            "g": 391,
+            "gS": 415,
+            "a": 440,
+            "aS": 455,
+            "b": 466,
+            "cH": 523,
+            "cHS": 554,
+            "dH": 587,
+            "dHS": 622,
+            "eH": 659,
+            "fH": 698,
+            "fHS": 740,
+            "gH": 784,
+            "gHS": 830,
+            "aH": 880,
+            "aHS": 910,
+            "bH": 933
+            }
+
 class Buzzer(object):
  def __init__(self):
   GPIO.setmode(GPIO.BCM)  
@@ -33,40 +74,14 @@ class Buzzer(object):
   x=0
 
   print("Playing tune ",tune)
-  if(tune==1):
-    pitches=[262,294,330,349,392,440,494,523, 587, 659,698,784,880,988,1047]
-    duration=0.1
-    for p in pitches:
-      self.buzz(p, duration)  #feed the pitch and duration to the function, "buzz"
-      time.sleep(duration *0.5)
-    for p in reversed(pitches):
-      self.buzz(p, duration)
-      time.sleep(duration *0.5)
-
-  elif(tune==2):
-    pitches=[262,330,392,523,1047]
-    duration=[0.2,0.2,0.2,0.2,0.2,0,5]
-    for p in pitches:
-      self.buzz(p, duration[x])  #feed the pitch and duration to the function, "buzz"
-      time.sleep(duration[x] *0.5)
-      x+=1
-  elif(tune==3):
+  if(tune=="sitting"):
     pitches=[392,294,0,392,294,0,392,0,392,392,392,0,1047,262]
     duration=[0.2,0.2,0.2,0.2,0.2,0.2,0.1,0.1,0.1,0.1,0.1,0.1,0.8,0.4]
     for p in pitches:
       self.buzz(p, duration[x])  #feed the pitch and duration to the func$
       time.sleep(duration[x] *0.5)
       x+=1
-
-  elif(tune==4):
-    pitches=[1047, 988,659]
-    duration=[0.1,0.1,0.2]
-    for p in pitches:
-      self.buzz(p, duration[x])  #feed the pitch and duration to the func$
-      time.sleep(duration[x] *0.5)
-      x+=1
-
-  elif(tune==5):
+  elif(tune=="posture"):
     pitches=[1047, 988,523]
     duration=[0.1,0.1,0.2]
     for p in pitches:
@@ -74,9 +89,10 @@ class Buzzer(object):
       time.sleep(duration[x] *0.5)
       x+=1
 
+
   GPIO.setup(self.buzzer_pin, GPIO.IN)
 
 if __name__ == "__main__":
-  a = input("Enter Tune number 1-5:")
+  a = input("Enter \"posture\" or \"sitting\":")
   buzzer = Buzzer()
-  buzzer.play(int(a))
+  buzzer.play(str(a))
